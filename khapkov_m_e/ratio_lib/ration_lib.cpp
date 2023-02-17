f5e837925e16fc4bd8ce5dd15233424588fc07be
Хапков Михаил <74322884+L4dno@users.noreply.github.com>
2023-02-17 10:59:48 +0300

    Add files via upload

---
+++ khapkov_m_e/ratio_lib/ration_lib.cpp
@@ -0,0 +1,44 @@
+﻿// ration_lib.cpp : Defines the entry point for the application.
+//
+
+#include "ration_lib.h"
+#include <cassert>
+#include <exception>
+
+Ratio::Ratio(int num, int denom) :
+    numerator(num), denominator(denom) {
+    Reduce();
+
+void Ratio::Test() {
+
+    assert(GCD(3, 15) == 3);
+    assert(GCD(7, 4) == 1);
+    assert(GCD(3, 0) == 1);
+
+    assert(GCD(denominator, numerator)==1);
+
+}
+
+
+void Ratio::Reduce() {
+
+    if (denominator == 0) {
+        throw std::divisi
+    }
+
+    int gcd = GCD(numerator, denominator);
+    numerator /= gcd;
+    denominator /= gcd;
+}
+
+int Ratio::LCM(int lhs, int rhs) {
+    return lhs * rhs / GCD(lhs, rhs);
+}
+
+int Ratio::GCD(int lhs, int rhs) {
+    while (rhs != 0) {
+        lhs %= rhs;
+        std::swap(lhs, rhs);
+    }
+    return lhs;
+}
---
+++ khapkov_m_e/ratio_lib/ration_lib.h
@@ -0,0 +1,40 @@
+﻿// ration_lib.h : Include file for standard system include files,
+// or project specific include files.
+
+#pragma once
+
+#include <iostream>
+
+// TODO: Reference additional headers your program requires here.
+
+class Ratio {
+
+public:
+
+    Ratio() = default;
+    Ratio(int num, int denom);
+    explicit Ratio(double fraction);
+
+    bool operator==(const Ratio rhs) const;
+    bool operator!=(const Ratio rhs) const;
+
+    Ratio& operator+=(const Ratio rhs);
+    Ratio& operator-=(const Ratio rhs);
+    Ratio& operator*=(const Ratio rhs);
+    Ratio& operator/=(const Ratio rhs);
+
+    void Test();
+
+private:
+
+    void Reduce();
+
+    int LCM(int lhs, int rhs);
+    int GCD(int lhs, int rhs);
+
+    int numerator = 0;
+    int denominator = 1;
+
+    //static const double eps = 1e-7;
+
+};
